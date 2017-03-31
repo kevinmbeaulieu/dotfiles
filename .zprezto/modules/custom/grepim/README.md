f83ba2dd9713a1eb4fd339b8559824bf4456a5be
Kevin Beaulieu <kevinmbeaulieu@gmail.com>
2017-03-31 15:30:59 -0400

    Create grepim

---
+++ .zprezto/modules/custom/grepim/README.md
@@ -0,0 +1,5 @@
+Use Spotlight to speed up `grep` by only applying `grep` to files that contain the desired string.
+
+Usage: `grepim query [dir/file] ...`
+
+Note: Since Spotlight does not index hidden files/directories, `grepim` will ignore them too (normal `grep` does not ignore them).
---
+++ .zprezto/modules/custom/grepim/init.zsh
@@ -0,0 +1,20 @@
+# Spotlight metadata find and grep by Daniel.Hoherd at gmail dot com
+function grepim() {
+    # Check for at least two arguments, print usage if else
+    if [ $# -lt 1 ] ; then
+        echo "Usage: $0 query [dir/file] ..."
+        exit ;
+    fi
+
+    ss=$1;
+    shift;
+    files=$@;
+
+    until [ -z "$1" ] ; do
+        thisitem=$1
+        onlyin="-onlyin '$thisitem' $onlyin"
+        shift
+    done;
+
+    eval mdfind -0 $onlyin "$ss" | xargs -0 grep -Hi "$ss"
+}
