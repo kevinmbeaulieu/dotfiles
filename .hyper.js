ad4a870df252a1b62f9d07303c925e16d2c89b52
Kevin Beaulieu <kevinmbeaulieu@gmail.com>
2017-04-12 11:24:15 -0400

    Clean up Hyper config

--- .hyper.js
+++ .hyper.js
@@ -1,132 +1,37 @@
-// Future versions of Hyper may add additional config options,
-// which will not automatically be merged into this file.
-// See https://hyper.is#cfg for all currently supported options.
-
 module.exports = {
-  config: {
-    // default font size in pixels for all tabs
-    fontSize: 12,
+    config: {
+        fontSize: 12,
+        fontFamily: 'FiraCode-Retina, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+        cursorColor: 'rgba(68, 243, 240, 0.7)',
+        cursorShape: 'UNDERLINE',
+        cursorBlink: false,
+        foregroundColor: '#fff',
+        backgroundColor: 'rgb(58, 61, 69)',
+        termCSS: `
+            x-screen x-row {
+                font-variant-ligatures: initial; /* enable ligatures */
+            }
+        `,
+        padding: '8px 10px',
+        shell: '/bin/zsh',
+        shellArgs: ['--login'],
+        env: {},
+        bell: false,
+        copyOnSelect: true,
 
-    // font family with optional fallbacks
-    fontFamily: 'FiraCode-Retina, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+        hyperBorder: {
+            borderColors: ['#fc1da7', '#fba506'],
+            borderWidth: '2px'
+        },
 
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(68, 243, 240, 0.7)',
-
-    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: 'UNDERLINE',
-
-    // set to true for blinking cursor
-    cursorBlink: false,
-
-    // color of the text
-    foregroundColor: '#fff',
-
-    // terminal background color
-    backgroundColor: 'rgb(58, 61, 69)',
-
-    // border color (window, tabs)
-//    borderColor: '#f1c02e',
-
-    // custom css to embed in the main window
-    css: '',
-
-    // custom css to embed in the terminal window
-    termCSS: `
-        x-screen x-row {
-            font-variant-ligatures: initial;
-        }
-    `,
-
-    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
-    // default: `false` on Linux, `true` on Windows (ignored on macOS)
-    showHamburgerMenu: '',
-
-    // set to `false` if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` on windows and Linux (ignored on macOS)
-    showWindowControls: '',
-
-    // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
-
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
-    colors: {
-      black: '#000000',
-      red: '#ff0000',
-      green: '#33ff00',
-      yellow: '#ffff00',
-      blue: '#0066ff',
-      magenta: '#cc00ff',
-      cyan: '#00ffff',
-      white: '#d0d0d0',
-      lightBlack: '#808080',
-      lightRed: '#ff0000',
-      lightGreen: '#33ff00',
-      lightYellow: '#ffff00',
-      lightBlue: '#0066ff',
-      lightMagenta: '#cc00ff',
-      lightCyan: '#00ffff',
-      lightWhite: '#ffffff'
     },
 
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    // make sure to use a full path if the binary name doesn't work
-    // (e.g `C:\\Windows\\System32\\bash.exe` instead of just `bash.exe`)
-    // if you're using powershell, make sure to remove the `--login` below
-    shell: '/bin/zsh',
+    plugins: [
+        'hypercwd',
+        'hyperlinks',
+        'hyperterm-safepaste',
+        'hyperborder',
 
-    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
-    // by default ['--login'] will be used
-    shellArgs: ['--login'],
-
-    // for environment variables
-    env: {},
-
-    // set to false for no bell
-    bell: false,
-
-    // if true, selected text will automatically be copied to the clipboard
-    copyOnSelect: true,
-
-    // if true, on right click selected text will be copied or pasted if no
-    // selection is present (true by default on Windows)
-    // quickEdit: true
-
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
-
-    // for advanced config flags please refer to https://hyper.is/#cfg
-    
-    hyperBorder: {
-        borderColors: ['#fc1da7', '#fba506'],
-        borderWidth: '2px'
-    },
-
-  },
-
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
-  plugins: [
-      'hypercwd',
-      'hyperlinks',
-      'hyperterm-safepaste',
-      'hyperborder',
-
-      'hyper-atom-dark-transparent',
-  ],
-
-
-  // in development, you can create a directory under
-  // `~/.hyper_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
-  localPlugins: []
-
+        'hyper-atom-dark-transparent',
+    ]
 };
