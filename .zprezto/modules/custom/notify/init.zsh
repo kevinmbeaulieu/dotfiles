59b6be993646ea61b99e7688d597cbfc2cc17c39
Kevin Beaulieu <kevinb@thumbtack.com>
2017-07-07 15:01:01 -0700

    Add support to notify module for custom messages

--- .zprezto/modules/custom/notify/init.zsh
+++ .zprezto/modules/custom/notify/init.zsh
@@ -1,3 +1,8 @@
 function notify() {
-    osascript -e 'display notification "Command finished." with title "Terminal"'
+    if [ $# -ge 1 ]
+    then
+        osascript -e "display notification \"$1\" with title \"Terminal\""
+    else
+        osascript -e "display notification \"Command finished.\" with title \"Terminal\""
+    fi
 }
